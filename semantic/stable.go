9818ac247d71828e03205bf277c3dbbb6326fa88
Samer Masterson <samer@samertm.com>
2014-07-17 12:40:02 -0400

    Added NodeInfo, types

--- semantic/stable.go
+++ semantic/stable.go
@@ -2,9 +2,58 @@ package semantic
 
 import "log"
 
+// Let's create a type to hold information about the variables in
+// our program.
+type NodeInfo struct {
+	// What variables do we need? Probably a pointer to a type
+	T Type
+	// What else? We don't need the identifier name because
+	// that's stored in the symbol table. There may be other
+	// things but I'm not sure what they are.
+}
+
+// Okay, let's create our Type type. Type will hold all the
+// information we need to generate code for a specific type.
+interface Type  {
+	Equal(Type) bool
+}
+
+type Func struct {
+	Args []Type
+}
+
+func (f *Func) Equal(t Type) bool {
+	fn, ok := t.(*Func)
+	if !ok {
+		return false
+	}
+	if len(f.Args) != len(fn.Args) [
+		return false
+	}
+	for i := 0; i < len(f.Args); i++ {
+		if f.Args[i].Equal(fn.Args[i]) == false {
+			return false
+		}
+	}
+	return true
+}
+
+func Basic struct {
+	Name string
+	// this is a pointer type if true
+	Pointer bool
+}
+
+func (b *Basic) Equal(t Type) bool {
+	ba, ok := t.(*Basic)
+	if !ok {
+		return false
+	}
+	return b.Name == ba.Name && b.Pointer == ba.Pointer
+}
+
 type stable struct {
-	// change interface{} to something else?
-	scopes []map[string]interface{}
+	scope []map[string]NodeInfo
 }
 
 func NewStable() *stable {

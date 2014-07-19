e4836859c0594fcf505dcbab59893cdd6f817c1a
Samer Masterson <samer@samertm.com>
2014-07-19 01:36:48 -0400

    Turned fgen into a stub

--- semantic/fgen.go
+++ semantic/fgen.go
@@ -4,26 +4,7 @@ package semantic
 
 import "github.com/samertm/chompy/parse"
 
-func Fgen(t parse.Node) string {
-	// first: check that it's a tree
-	tree, err := parse.AssertTree(t)
-	if err != nil {
-		return err.Error()
-	}
-	if tree.Kids[2].Valid() == false {
-		f, _ := parse.AssertFuncdecl(tree.Kids[2])
-		if f.FuncOrSig.Valid() == false {
-			fun, err := parse.AssertFunc(f.FuncOrSig)
-			if err != nil {
-				return err.Error()
-			}
-			if fun.Sig.(*parse.Sig).Result == nil {
-				return "HI"
-			}
-			return fun.String()
-		}
-		return f.String()
-	}
-	// second: check for a package statement
+// stub
+func fgen(t parse.Node) string {
 	return "yay :D"
 }
